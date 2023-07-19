283ec7759ad06a308dd8fd23f981328873684285
Anchal <achalvishwakarma00@gmail.com>
2023-07-19 15:48:08 +0530

    Firts commit

---
+++ Basic/Comments/Documentation.dart
@@ -0,0 +1,17 @@
+bool checkEven(n){
+/// Returns true
+/// if n is even
+if(n%8==0)
+
+	return true;
+/// Returns false if n is odd
+else
+
+	return false; }
+
+int main()
+{
+int n = 55;
+print(checkEven(n));
+return 0;
+}
---
+++ Data type/Dart data/List.dart
@@ -0,0 +1,10 @@
+void main()
+{
+	List gfg = new List(3);
+	gfg[0] = 'welcome';
+	gfg[1] = 'to';
+	gfg[2] = 'university';
+	
+	print(gfg);
+	print(gfg[0]);
+}
