e7163484cdef5a8771698190141dd26d347726ee
aachalvishwakarma00 <124452413+aachalvishwakarma00@users.noreply.github.com>
2023-08-12 16:18:24 +0530

    Delete Dart directory

--- Dart/Dart program/Check String is Empty or Not in Dart (Null Safety)/check.dart
+++
@@ -1,9 +0,0 @@
-// main function start
-void main() {
-
-	// initialise a string st
-	String? st = "Welcome to University";
-
-	// print string is empty or not
-	print(st.isEmpty);
-}
--- Dart/Key Functions/main() Function/argument.dart
+++
@@ -1,5 +0,0 @@
-main(List<String> arguments){
-	//printing the arguments along with length
-	print(arguments.length);
-	print(arguments);
-}
